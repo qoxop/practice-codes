32cbd48a3a7d2c7556fe0b11a18d1ec7b0e5119b
qoxop <qoxopzhang@outlook.com>
2020-08-28 14:04:27 +0800

    自动提交

---
+++ javascript-algorithms/math/euclidean-algorithm/euclideanAlgorithmIterative.ts
@@ -0,0 +1,19 @@
+/**
+ * 欧几里得算法，求最大公约数
+ * 迭代版
+ */
+function euclideanAlgorithmIterative(A, B) {
+    A = Math.abs(A);
+    B = Math.abs(B);
+    let b = A % B;
+    while(b) {
+      [A, B] = [B, b];
+      b = A % B;
+    }
+    // 不用额外变量的方式
+    // while(A && B && A !== B) { 
+    //   [A, B] = [B, A%B];
+    // }
+    // return B || A;
+    return B;
+  }
