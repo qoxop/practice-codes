a29ec09b435f06778432ae492c61aa444f212eed
zhangjw <qoxopzhang@outlook.com>
2020-08-29 19:38:07 +0800

    自动提交

--- javascript-algorithms/math/index.ts
+++ javascript-algorithms/math/index.ts
@@ -3,7 +3,7 @@ import trialDivision from './trialDivision/trialDivision'
 
 const arr = sieveOfEratosthenes(10000)
 
-const nArr = new Array<boolean>(10001).fill(true);
+const nArr = new Array<boolean>(10001)
 
 if (nArr.filter((_, index) => trialDivision(index)).length === arr.length) {
     console.log('sss');
