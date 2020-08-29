475e3b99084d7ce4fd7ec6c2fa819d3769a76683
zhangjw <qoxopzhang@outlook.com>
2020-08-29 19:05:56 +0800

    自动提交

--- javascript-algorithms/math/index.ts
+++ javascript-algorithms/math/index.ts
@@ -1,7 +1,9 @@
 import sieveOfEratosthenes from './sieveOfEratosthenes/sieveOfEratosthenes';
 import trialDivision from './trialDivision/trialDivision'
 
-if (sieveOfEratosthenes(1000).every(item => trialDivision(item))) {
+const arr = sieveOfEratosthenes(10000)
+console.log(arr)
+if (arr.every(item => trialDivision(item))) {
     console.log('success')
 } else {
     console.log('fail')
