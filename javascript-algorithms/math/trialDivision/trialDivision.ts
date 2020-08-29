94e5c54b86fc614dd574a777497b0ecec2ef3136
zhangjw <qoxopzhang@outlook.com>
2020-08-29 19:32:36 +0800

    自动提交

--- javascript-algorithms/math/trialDivision/trialDivision.ts
+++ javascript-algorithms/math/trialDivision/trialDivision.ts
@@ -5,23 +5,25 @@
  * @param num 
  */
 export default function trialDivision(num: number): boolean {
-    // 特殊数字
-    if (num === 2 || num === 3) return true;
+    // // 特殊数字
+    // if (num === 2 || num === 3) return true;
 
-    // 排除非整数、偶数 
-    if (num % 1 !== 0 || num % 2 === 0) return false;
+    // // 排除非整数、偶数 
+    // if (num % 1 !== 0 || num % 2 === 0) return false;
 
-    // 取平方根
-    const dive = Math.sqrt(num);
+    // // 取平方根
+    // const dive = Math.sqrt(num);
 
-    // 平方根不为整数
-    if (dive % 1 === 0) return false;
+    // // 平方根不为整数
+    // if (dive % 1 === 0) return false;
 
-    // 假设 num 是一个矩形的面积， num = a * b; a、b均为正整数，a 从一个开始递增，b从num开始递减，a = b 时
-    // a = b = dive，过了这个点，a 将大于 b，a 开始变成 b 之前经历过的值(此时 b 同理)，当 a 超过dive的部分为重复的
-    for(let d = 3; d <= dive; d += 2) {
-        if (num % d === 0) return false;
-    }
-
+    // // 假设 num 是一个矩形的面积， num = a * b; a、b均为正整数，a 从一个开始递增，b从num开始递减，a = b 时
+    // // a = b = dive，过了这个点，a 将大于 b，a 开始变成 b 之前经历过的值(此时 b 同理)，当 a 超过dive的部分为重复的
+    // for(let d = 3; d <= dive; d += 2) {
+    //     if (num % d === 0) return false;
+    // }
+    if (num === 2 || num === 3 || num === 5) return true;
+    if (num % 1 !== 0 || num % 2 === 0 || num % 3 === 0 || num % 5 === 0) return false;
+    if (Math.sqrt(num) % 1 === 0) return false;
     return true;
 }
