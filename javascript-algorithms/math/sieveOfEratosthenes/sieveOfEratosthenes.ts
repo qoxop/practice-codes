a0e7a6d27eea34b28e61f1387bfd23bb6c6db3f3
zhangjw <qoxopzhang@outlook.com>
2020-08-29 18:20:54 +0800

    自动提交

--- javascript-algorithms/math/sieveOfEratosthenes/sieveOfEratosthenes.ts
+++ javascript-algorithms/math/sieveOfEratosthenes/sieveOfEratosthenes.ts
@@ -7,8 +7,8 @@ function sieveOfEratosthenes(max: number): number[] {
     const bArr = [];
     const dive = Math.sqrt(max);
     for (let p = 2; p <= dive; p++) {
-        
-        if (bArr[p] === true) continue;
+        // 跳过已经至为 false 的数
+        if (bArr[p] === false) continue;
         let s = p;
         let m = s * p;
         while(m < max) {
