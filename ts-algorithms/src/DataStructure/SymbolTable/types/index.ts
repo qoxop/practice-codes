8aa36857f37016ae409184203733a1037505795b
zhangjw <qoxopzhang@outlook.com>
2020-09-20 19:00:45 +0800

    自动提交

--- ts-algorithms/src/DataStructure/SymbolTable/types/index.ts
+++ ts-algorithms/src/DataStructure/SymbolTable/types/index.ts
@@ -8,7 +8,6 @@ interface ST<Key extends Comparable<Key>, Value> {
     delete(key: Key): void;
     contains(key: Key): boolean;
     isEmpty(): boolean;
-    size(node?: any): number;
     min(): Key;
     max(): Key;
     floor(key: Key): Key; // 小于等于key的最大键
@@ -17,7 +16,6 @@ interface ST<Key extends Comparable<Key>, Value> {
     select(k: number): Key; // 排名为 k 的键
     deleteMin(): void;
     deleteMax(): void;
-    size(lo: Key, hi: Key): number;
-    keys(lo: Key, hi: Key): Iterable<Key>;
-    keys(): Iterable<Key>;
+    size(lo?: Key, hi?: Key): number;
+    keys(lo?: Key, hi?: Key): Iterable<Key>;
 }
