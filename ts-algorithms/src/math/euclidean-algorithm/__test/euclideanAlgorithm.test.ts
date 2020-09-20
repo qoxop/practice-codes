2094dc262b8332f4b0f6df301c014146928e0cf2
zhangjw <qoxopzhang@outlook.com>
2020-09-20 18:04:23 +0800

    自动提交

--- ts-algorithms/src/math/euclidean-algorithm/__test/euclideanAlgorithm.test.ts
+++ ts-algorithms/src/math/euclidean-algorithm/__test/euclideanAlgorithm.test.ts
@@ -1,5 +1,5 @@
 import euclideanAlgorithm from '../euclideanAlgorithm';
 
-test('99 和 66的最大公约数 等于 33', () => {
+test('99 和 66 的最大公约数 等于 33', () => {
     expect(euclideanAlgorithm(99, 66)).toBe(33)
 })
