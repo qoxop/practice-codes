8253454a0eea99f269356217ff6671a1e12152ca
zhangjw <qoxopzhang@outlook.com>
2020-09-20 15:10:58 +0800

    自动提交

--- ts-algorithms/src/DataStructure/SymbolTable/RedBlackBST.ts
+++ ts-algorithms/src/DataStructure/SymbolTable/RedBlackBST.ts
@@ -1,6 +1,24 @@
 /**
- * 红黑树的ts实现
+ * 红黑树符号表的ts实现
  */
-interface RedBlackBST {
-    put
+class Node<Key extends Comparable, Value> {
+    static BLACK = false;
+    static RED = false;
+    key: Key;
+    value: Value;
+    left?: Node<Key, Value>;
+    right?: Node<Key, Value>;
+    color: boolean;
+    size: number;
+    constructor(key: Key, value: Value, color: boolean) {
+        this.key = key;
+        this.value = value;
+        this.color = color;
+    }
+}
+class RedBlackBST<Key extends Comparable, Value> implements ST<Key, Value> {
+    private Node = {}
+    put(key: Key, value: Value) {
+
+    }
 }
